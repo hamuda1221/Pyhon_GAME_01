5cebb50c06e4a5e4b278dd8dc2dc33923389136a
Tomoaki.Hamada <j316231@ns.kogakuin.ac.jp>
2018-06-07 23:41:59 +0900

    ブロックの描画

--- game_01.py
+++ game_01.py
@@ -22,15 +22,30 @@ class Paddle(pygame.sprite.Sprite):
     def update(self):
         self.rect.centerx = pygame.mouse.get_pos()[0]   #マウスのx座標をパドルのx座標に
         self.rect.clamp_ip(SCREEN)
+ 
+#ブロックのクラス
+class Block(pygame.sprite.Sprite):
+    def __init__(self, filename, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = pygame.image.load(filename).convert()
+        self.rect = self.image.get_rect()
+        self.rect.left = SCREEN.left + x * self.rect.width
+        self.rect.top = SCREEN.top + y * self.rect.height
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN.size)
     group = pygame.sprite.RenderUpdates()       #描画用のスプライトグループ
+    blocks = pygame.sprite.Group()              #衝突判定用のスプライトグループ
     Paddle.containers = group
+    Block.containers = group, blocks
     paddle = Paddle("paddle.png")               #パドルの作成
+    #ブロックの生成（14 * 10）
+    for x in range(1, 15):
+        for y in range(1, 11):
+            Block("block.png", x, y)
     clock = pygame.time.Clock()
-    
+
     while(1):
         clock.tick(60)      #フレームレート(60fps)
         screen.fill((0, 20, 0))
